bebb1bec1e9e15217a16d7052e87f3a812ec0279
Mateo Blandon Mesa <matius-b@hotmail.com>
2023-04-13 14:24:09 -0500

    I added log4net Nuget package, configured it to show exception in console and insert exception in log databse.

--- TextFileDataAccessDemo/ConsoleUI/Program.cs
+++ TextFileDataAccessDemo/ConsoleUI/Program.cs
@@ -3,13 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+
+[assembly: log4net.Config.XmlConfigurator(Watch = true)]
 
 namespace ConsoleUI
 {
-  internal class Program
+  class Program
   {
+    public static readonly log4net.ILog log = LogHelper.GetLogger();
     static void Main(string[] args)
     {
-    }
+      try
+      {
+        string filePath = @"C:\Documents\Cursos\TimCorey\C-Sharp-Data-Access-Text-Files\Demos\Test.txt";
+        List<string> lines = File.ReadAllLines(filePath).ToList();
+        foreach (string line in lines)
+        {
+          Console.WriteLine(line);
+        }
+        Console.ReadLine();
+      }
+      catch (Exception ex) 
+      { 
+        log.Debug("Ha ocurrido una excepción:", ex
+          );
+        Console.ReadLine();
+      }
+     }
   }
 }
